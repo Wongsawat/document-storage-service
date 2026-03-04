9c66d81a3a371f0b4207e99f3105834beee8709d
Weerachat Wongsawat <rabbit_roger@yahoo.com>
2026-03-04 18:49:36 +0700

    feat: add OutboxRepositoryPort interface

---
+++ src/main/java/com/wpanther/storage/domain/port/outbound/OutboxRepositoryPort.java
@@ -0,0 +1,12 @@
+package com.wpanther.storage.domain.port.outbound;
+
+import com.wpanther.saga.commons.outbox.OutboxEvent;
+
+/**
+ * Outbound port for outbox event persistence.
+ * Implemented by JpaOutboxAdapter.
+ */
+public interface OutboxRepositoryPort {
+    OutboxEvent save(OutboxEvent event);
+    void deleteById(String id);
+}
